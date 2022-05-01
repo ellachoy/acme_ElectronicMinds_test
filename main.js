15479db558414a1309c0d39fb08048895a5762b2
ella choy <egal21de@gmail.com>
2022-05-01 17:06:33 +0200

    recommit

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const clamp= document.querySelector('.clamp');
 moreBtn.addEventListener('click',()=>{
     clamp.classList.toggle('clamp');
 });
+
+// Navbar toggle button for mobile version
+const navbarMenu = document.querySelector('.navbar__menu');
+const navbarToggleBtn= document.querySelector('navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    console.log('test');
+})
+// navbarToggleBtn.addEventListener('click',()=>{
+//     console.log('test');
+//     //  navbarMenu.classList.toggle('open');
+// });
